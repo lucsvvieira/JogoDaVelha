ea04e4a4f70c7e2d8b96b114f9c706339695f959
Lucas Vieira <vieira07lucas@gmail.com>
2023-07-18 15:44:17 -0300

    Fazendo a Inteligência Artificial jogar

--- js/scripts.js
+++ js/scripts.js
@@ -27,6 +27,14 @@ for(let i = 0; i < boxes.length; i++) {
             // Computar jogada
             if(player1 == player2) {
                 player1++;
+
+                if(secondPlayer == 'ai-player') {
+
+                    // Função para executar a jogada
+                    computerPlay();
+                    player2++;
+
+                }
             } else {
                 player2++;
             }
@@ -38,6 +46,28 @@ for(let i = 0; i < boxes.length; i++) {
 
     });
     
+}
+
+// Evento para saber se é 2 players ou Inteligencia Artificial
+for(let i = 0; i < buttons.length; i++) {
+
+    buttons[i].addEventListener("click", function() {
+
+        secondPlayer = this.getAttribute("id");
+
+        for(let j = 0; j < buttons.length; j++) {
+            buttons[j].style.display = 'none';
+        }
+
+        setTimeout(function() {
+
+            let container = document.querySelector("#container");
+            container.classList.remove("hide");
+
+        }, 500);
+
+    });
+
 }
 
 // Verifica quem vai jogar
@@ -248,6 +278,37 @@ function declareWinner(winner) {
 
     for(let i = 0; i < boxesToRemove.length; i++) {
         boxesToRemove[i].parentNode.removeChild(boxesToRemove[i]);
+    }
+
+}
+
+// Executar a lógica de jogada da CPU
+function computerPlay() {
+    
+    let cloneO = o.cloneNode(true);
+    counter = 0;
+    filled = 0;
+
+    for(let i = 0; i < boxes.length; i++) {
+
+        let randomNumber = Math.floor(Math.random() * 5);
+
+        // Só preencher se estiver vazio o filho
+        if(boxes[i].childNodes[0] == undefined) {
+            if(randomNumber <= 1) {
+                boxes[i].appendChild(cloneO);
+                counter++;
+                break;
+            }
+        // Checagem de quantos estão preenchidas
+        } else {
+            filled++;
+        }
+        
+    }
+
+    if(counter == 0 && filled < 9) {
+        computerPlay();
     }
 
 }
